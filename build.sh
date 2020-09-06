6a49972d786446ce22843c01c6daa9f89a6b92d2
conni <cbuschka@gmail.com>
2020-09-06 16:05:25 +0200

    Fixed manifest handling.

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ trap "exit 1" SIGHUP
 cd /work
 rm -rf /work/target && mkdir -p /work/target/classes
 cd /work/src
-javac -d /work/target/classes/ ./graaleval/Main.java
+javac -d /work/target/classes/ $(find . -name '*.java')
 cd /work/target/classes
-mkdir -p /work/target/classes/META-INF
-cat - >/work/target/classes/META-INF/MANIFEST.MF <<EOB
+mkdir -p /work/target/classes
+cat - >/work/target/manifest.mf <<EOB
 Main-Class: graaleval.Main
 EOB
-jar cvfm /work/target/app.jar /work/target/classes/META-INF/MANIFEST.MF .
+jar cvfm /work/target/app.jar /work/target/manifest.mf .
 jar tvf /work/target/app.jar
 cd /work/target
 native-image --enable-url-protocols=http \
@@ -21,4 +21,5 @@ native-image --enable-url-protocols=http \
                       -H:+ReportUnsupportedElementsAtRuntime --no-server -jar /work/target/app.jar
 
 #                      -H:ReflectionConfigurationFiles=/work/reflect.json \
+
 
