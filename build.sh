27c484e669d547ac9119e4af8b6f82be35f1be1b
conni <cbuschka@gmail.com>
2020-11-15 19:31:47 +0100

    Changed build.sh to work independent of working dir.

--- build.sh
+++ build.sh
@@ -4,22 +4,23 @@ trap "exit 1" SIGINT
 trap "exit 1" SIGTERM
 trap "exit 1" SIGHUP
 
-cd /work
-rm -rf /work/target && mkdir -p /work/target/classes
-cd /work/src
-javac -d /work/target/classes/ $(find . -name '*.java')
-cd /work/target/classes
-mkdir -p /work/target/classes
-cat - >/work/target/manifest.mf <<EOB
+WORK_DIR=$(cd `dirname 0` && pwd)
+
+rm -rf $WORK_DIR/target && mkdir -p $WORK_DIR/target/classes
+cd $WORK_DIR/src
+javac -d $WORK_DIR/target/classes/ $(find . -name '*.java')
+cd $WORK_DIR/target/classes
+mkdir -p $WORK_DIR/target/classes
+cat - >$WORK_DIR/target/manifest.mf <<EOB
 Main-Class: graaleval.Main
 EOB
-jar cvfm /work/target/app.jar /work/target/manifest.mf .
-jar tvf /work/target/app.jar
-cd /work/target
+jar cvfm $WORK_DIR/target/app.jar $WORK_DIR/target/manifest.mf .
+jar tvf $WORK_DIR/target/app.jar
+cd $WORK_DIR/target
 native-image --enable-url-protocols=http \
                       -Djava.net.preferIPv4Stack=true \
-                      -H:+ReportUnsupportedElementsAtRuntime --no-server -jar /work/target/app.jar
+                      -H:+ReportUnsupportedElementsAtRuntime --no-server -jar $WORK_DIR/target/app.jar
 
-#                      -H:ReflectionConfigurationFiles=/work/reflect.json \
+#                      -H:ReflectionConfigurationFiles=$WORK_DIR/reflect.json \
 
 
